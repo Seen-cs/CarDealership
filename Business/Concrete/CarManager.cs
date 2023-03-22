b47b7217f4b519b9fd5153464ef51342120b5a91
Enes Doner <businessenesdoner@gmail.com>
2023-03-22 16:12:28 +0300

    HttpContext ile JWT üzerinden kullanıcı Id bulunması yapıldı.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
@@ -99,6 +100,8 @@ namespace Business.Concrete
                             smtp.Credentials = new NetworkCredential("seenbilgi@outlook.com", "123456789seen");
                             smtp.EnableSsl = true;
                             smtp.Send(mail);
+                            Task.Delay(5000);
+
                         }
                     }
                 }
